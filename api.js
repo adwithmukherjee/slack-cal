401240a41341413fb77f5a2afa834ae05122eed3
adwithmukherjee <adwithmukherjee@gmail.com>
2020-08-19 02:54:48 -0400

    dynamic name switching

--- api.js
+++ api.js
@@ -6,7 +6,7 @@ const callAPIMethod = async (method, payload) => {
     
         headers: { 
             Authorization: "Bearer xoxp-1301288377174-1306647213349-1315407075604-bbec7f6b4ebc601867f82ae4b5904e8d", 
-            'Content-Type': 'application/json; charset=utf-8'
+            //'Content-Type': 'application/x-www-form-urlencoded'
         }
     });
     return result.data;
--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const parseReq = (req, key) => {
     return data.slice(startIndex, -1).substring(0,endIndex)
 }
 
+const getOtherUser = (members) => {
+
+    const otherUser = otherUser = members[0] === user_id ? members[1] : members[0]; 
+    return otherUser
+}
 
 const rawBodyBuffer = (req, res, buf, encoding) => {
     if (buf && buf.length) {
@@ -25,18 +30,103 @@ app.use(bodyParser.urlencoded({ verify: rawBodyBuffer, extended: true }));
 app.use(bodyParser.json({ verify: rawBodyBuffer }));
 
 app.post('/test', async (req,res)=>{
-    const { channel_id } = req.body
-    console.log(channel_id)
+    const { channel_id, user_id } = req.body
+  
+
+    //const members = await axios.get(`https://slack.com/api/conversations.members?token=xoxp-1301288377174-1306647213349-1315407075604-bbec7f6b4ebc601867f82ae4b5904e8d&channel=${channel_id}&pretty=1`)
+
+
+    
+    const conversation_info = await axios.get(`https://slack.com/api/conversations.members?token=xoxp-1301288377174-1306647213349-1315407075604-bbec7f6b4ebc601867f82ae4b5904e8d&channel=${channel_id}&pretty=1`)
+    const { members } = conversation_info.data
+    console.log(members)
+    const otherUser = members.length === 1 ? members[0] : members[0] === user_id ? members[1] : members[0]; 
+    console.log("OTHER USER", otherUser)
+    let otherUserName = await axios.get(`https://slack.com/api/users.info?token=xoxp-1301288377174-1306647213349-1315407075604-bbec7f6b4ebc601867f82ae4b5904e8d&user=${otherUser}&pretty=1`)
+   
     const response = await callAPIMethod(
         "chat.postMessage",
 
         {
             "channel": channel_id,
-            "text": "Hello world! This is a test :tada:",
+            "blocks": [
+                {
+                  "type": "section",
+                  "text": {
+                    "type": "mrkdwn",
+                    "text": `Here's a list of common available meeting times! Select one to invite ${otherUserName.data.user.real_name} to.`
+                  }
+                },
+                {
+                  "type": "section",
+                  "block_id": "section567",
+                  "text": {
+                    "type": "mrkdwn",
+                    "text": "• 12:00pm (EST) Thursday, Aug 20, 2020"
+                  },
+                  "accessory": {
+                    "type": "button",
+                      "text": {
+                          "type": "plain_text",
+                          "text": "Send Invite",
+                          "emoji": false
+                      }
+                  }
+                },
+                {
+                    "type": "section",
+                    "block_id": "section568",
+                    "text": {
+                      "type": "mrkdwn",
+                      "text": "• 12:00pm (EST) Thursday, Aug 20, 2020 "
+                    },
+                    "accessory": {
+                      "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Send Invite",
+                            "emoji": false
+                        }
+                    }
+                },
+                {
+                    "type": "section",
+                    "block_id": "section569",
+                    "text": {
+                      "type": "mrkdwn",
+                      "text": "• 12:00pm (EST) Thursday, Aug 20, 2020 "
+                    },
+                    "accessory": {
+                      "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Send Invite",
+                            "emoji": false
+                        }
+                    }
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                      {
+                        "type": "button",
+                          "text": {
+                              "type": "plain_text",
+                              "text": "Load more Options",
+                              "emoji": false
+                          }
+                      }
+                    ]
+                  }
+               
+                
+              ]
+            }
            
-        }
+        
     )
-    console.log(response)
+            
+    
  
     res.end()
 })
