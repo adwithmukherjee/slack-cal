f3ef9f6a84544b1bbd94d6d7d606d258e550962e
adwithmukherjee <adwithmukherjee@gmail.com>
2020-08-20 01:56:55 -0400

    WIP: cal integration

--- index.js
+++ index.js
@@ -1,49 +1,205 @@
+//require("./calendar.js")
+
 const express = require("express");
 const axios = require("axios")
 const http = require("http");
 const bodyParser = require("body-parser")
 const app = express(); 
 const qs = require("qs")
+//const { authorize, listEvents, getAccessToken } = require("./calendar")
 const { callAPIMethod } = require("./api")
+const {google} = require('googleapis');
+const fs = require('fs');
 
-const parseReq = (req, key) => {
-    //returns the string between "key=" and the next "&"
-    const data = `${req}`
-    const startIndex = data.search(key)+key.length+1
-    const endIndex = data.slice(startIndex, -1).search("&")
-    return data.slice(startIndex, -1).substring(0,endIndex)
-}
+let auth = null; 
+const TOKEN_PATH = 'token.json';
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 
-const getOtherUser = (members) => {
-
-    const otherUser = otherUser = members[0] === user_id ? members[1] : members[0]; 
-    return otherUser
+const listEvents = (auth) => {
+    const calendar = google.calendar({version: 'v3', auth});
+    calendar.events.list({
+      calendarId: 'primary',
+      timeMin: (new Date()).toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime',
+    }, async (err, res) => {
+      if (err) return console.log('The API returned an error: ' + err);
+      const events = res.data.items;
+      console.log(events)
+      if (events.length) {
+        await callAPIMethod(
+            'chat.postMessage', 
+            {
+                "channel": 'D0190K38SG5',
+                "blocks": [
+                    {
+                      "type": "section",
+                      "text": {
+                        "type": "mrkdwn",
+                        "text": `Here's a list of common available meeting times! Select one to invite to.`
+                      }
+                    },
+                    {
+                      "type": "section",
+                      "block_id": "section567",
+                      "text": {
+                        "type": "mrkdwn",
+                        "text": `• ${events[0].summary}`
+                      },
+                      "accessory": {
+                        "type": "button",
+                          "text": {
+                              "type": "plain_text",
+                              "text": "Send Invite",
+                              "emoji": false
+                          }
+                      }
+                    },
+                    {
+                        "type": "section",
+                        "block_id": "section568",
+                        "text": {
+                          "type": "mrkdwn",
+                          "text": `• ${events[1].summary ? events[1].summary : "nothing"}`
+                        },
+                        "accessory": {
+                          "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Send Invite",
+                                "emoji": false
+                            }
+                        }
+                    },
+                    {
+                        "type": "section",
+                        "block_id": "section569",
+                        "text": {
+                          "type": "mrkdwn",
+                          "text": `• ${events[2].summary ? events[2].summary : "nothing"}`
+                        },
+                        "accessory": {
+                          "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Send Invite",
+                                "emoji": false
+                            }
+                        }
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                          {
+                            "type": "button",
+                              "text": {
+                                  "type": "plain_text",
+                                  "text": "Load more Options",
+                                  "emoji": false
+                              }
+                          }
+                        ]
+                      }
+                   
+                    
+                  ]
+            }
+        )
+      } else {
+        console.log('No upcoming events found.');
+      }
+    });
 }
 
 const rawBodyBuffer = (req, res, buf, encoding) => {
     if (buf && buf.length) {
       req.rawBody = buf.toString(encoding || 'utf8');
     }
-  };
+  }; 
   
 app.use(bodyParser.urlencoded({ verify: rawBodyBuffer, extended: true }));
 app.use(bodyParser.json({ verify: rawBodyBuffer }));
 
+app.get('/callback', (req,res) => {
+    const { code } = req.query
+    
+
+    auth.getToken(code, (err, token) => {
+        if (err) return console.error('Error retrieving access token', err);
+        auth.setCredentials(token);
+        // Store the token to disk for later program executions
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+          if (err) return console.error(err);
+          console.log('Token stored to', TOKEN_PATH);
+        });
+      });
+    listEvents(auth)
+    console.log("test")
+    res.redirect("slack://open")
+
+})
+
+
 app.post('/test', async (req,res)=>{
+    console.log(req.body)
+    
     const { channel_id, user_id } = req.body
   
-
-    //const members = await axios.get(`https://slack.com/api/conversations.members?token=xoxp-1301288377174-1306647213349-1315407075604-bbec7f6b4ebc601867f82ae4b5904e8d&channel=${channel_id}&pretty=1`)
-
+    //CALENDAR STUFF
 
     
+    
+
+    const sendAuthUrl = async (oAuth2Client) => {
+        const authUrl = oAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: SCOPES,
+          });
+        console.log(authUrl)
+        const response = await callAPIMethod(
+            "chat.postMessage", 
+            {
+                "channel": channel_id, 
+                "text": `<${authUrl}|First, click here to link google calendar. > Then, run /scheduler again to send event invites!`
+            }
+        )
+    }
+
+    const authorize = (credentials) => {
+        const {client_secret, client_id, redirect_uris} = credentials.web;
+
+        const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+
+        auth = oAuth2Client
+
+    // Check if we have previously stored a token.
+        fs.readFile(TOKEN_PATH, async (err, token) => {
+            if (err) return sendAuthUrl(oAuth2Client);
+            oAuth2Client.setCredentials(JSON.parse(token));
+            listEvents(oAuth2Client);
+        });
+    }
+
+    fs.readFile('credentials.json', (err, content) => {
+        if (err) return console.log('Error loading client secret file:', err);
+        // Authorize a client with credentials, then call the Google Calendar API.
+        authorize(JSON.parse(content));
+    });
+
+    
+    
+
+
+    //SLACK TEST STUFF
     const conversation_info = await axios.get(`https://slack.com/api/conversations.members?token=xoxp-1301288377174-1306647213349-1315407075604-bbec7f6b4ebc601867f82ae4b5904e8d&channel=${channel_id}&pretty=1`)
     const { members } = conversation_info.data
     console.log(members)
     const otherUser = members.length === 1 ? members[0] : members[0] === user_id ? members[1] : members[0]; 
     console.log("OTHER USER", otherUser)
     let otherUserName = await axios.get(`https://slack.com/api/users.info?token=xoxp-1301288377174-1306647213349-1315407075604-bbec7f6b4ebc601867f82ae4b5904e8d&user=${otherUser}&pretty=1`)
-   
+    /*
     const response = await callAPIMethod(
         "chat.postMessage",
 
@@ -125,6 +281,7 @@ app.post('/test', async (req,res)=>{
            
         
     )
+    */
             
     
  
